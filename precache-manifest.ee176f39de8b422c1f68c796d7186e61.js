1ed60510181d568cecdcfc561fe9f7cdd0026734
Aamer Desai <56724460+minm333@users.noreply.github.com>
2020-11-27 03:09:48 -0500

    Updates

---
+++ precache-manifest.ee176f39de8b422c1f68c796d7186e61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997ec79c70a5e664ef98ac0348e6f0fc",
+    "url": "/react-fcc-random-quote-machine/index.html"
+  },
+  {
+    "revision": "fa31f2fb39d5c5b6d124",
+    "url": "/react-fcc-random-quote-machine/static/css/main.e9830a13.chunk.css"
+  },
+  {
+    "revision": "b3e6e2f8674bc5424875",
+    "url": "/react-fcc-random-quote-machine/static/js/2.55bb37d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-fcc-random-quote-machine/static/js/2.55bb37d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa31f2fb39d5c5b6d124",
+    "url": "/react-fcc-random-quote-machine/static/js/main.98e7b52c.chunk.js"
+  },
+  {
+    "revision": "e839aad62571ac1a1e5f",
+    "url": "/react-fcc-random-quote-machine/static/js/runtime-main.a1a71c6b.js"
+  }
+]);
